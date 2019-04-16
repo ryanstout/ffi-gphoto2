5354e517f6ef4e0c9c383f5d4518909dd2dd2c91
Ryan Stout <ryanstout@gmail.com>
2019-04-15 18:05:31 -0600

    udpate parent

--- lib/gphoto2/camera/configuration.rb
+++ lib/gphoto2/camera/configuration.rb
@@ -148,6 +148,7 @@ module GPhoto2
         if (old_widget = self[key])
           puts "OLD WIDGET: #{old_widget.inspect} vs NEW: #{widget.inspect}"
           # Replace it with the more recent one
+          widget.parent = old_widget.parent
           @config[key] = widget
 
           # Free the old widget
